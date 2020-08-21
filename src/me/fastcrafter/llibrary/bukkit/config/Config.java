86bad280c8a79983f0739b1efc7edae40144a1c6
FastCrafter <bjarne.scheid@cato-achim.de>
2020-08-21 21:26:51 +0200

    Update Config.java
    Added more Constructors
    Added copyDefaults() method. (not working at the moment!)

--- src/me/fastcrafter/llibrary/bukkit/config/Config.java
+++ src/me/fastcrafter/llibrary/bukkit/config/Config.java
@@ -1,40 +1,123 @@
 package me.fastcrafter.llibrary.bukkit.config;
 
-import org.bukkit.Bukkit;
+import me.fastcrafter.llibrary.bukkit.util.MSGUtils;
 import org.bukkit.configuration.file.FileConfiguration;
 import org.bukkit.configuration.file.YamlConfiguration;
+import org.bukkit.plugin.java.JavaPlugin;
 
 import java.io.File;
 import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.Reader;
+import java.nio.charset.StandardCharsets;
 
 public class Config {
-    private  File file;
-    private String filename;
-    private  FileConfiguration config;
+    private File file;
+    private final String filename;
+    private final File folder;
+    private FileConfiguration config;
+    private final MSGUtils msgInstance;
+    private final JavaPlugin plugin;
 
-    public Config(String filename, boolean setup) {
+    public Config(JavaPlugin plugin, MSGUtils msgInstance, String filename, boolean setup, boolean copyDefaults) {
+        this.msgInstance = msgInstance;
+        this.plugin = plugin;
         this.filename = filename;
+        this.folder = plugin.getDataFolder();
         if (setup)
             setup();
+        if (copyDefaults)
+            copyDefaults();
     }
 
-    public  void setup() {
-        file = new File(Bukkit.getServer().getPluginManager().getPlugin(filename).getDataFolder(), "customconfig.yml");
+    public Config(JavaPlugin plugin, MSGUtils msgInstance, String filename, boolean setup, boolean copyDefaults, String defaultLocation) {
+        this.msgInstance = msgInstance;
+        this.plugin = plugin;
+        this.filename = filename;
+        this.folder = plugin.getDataFolder();
+        if (setup)
+            setup();
+        if (copyDefaults)
+            copyDefaults(defaultLocation);
+
+    }
+
+    public Config(JavaPlugin plugin, MSGUtils msgInstance, String path, String filename, boolean setup, boolean copyDefaults) {
+        this.msgInstance = msgInstance;
+        this.plugin = plugin;
+        this.filename = filename;
+        this.folder = new File(plugin.getDataFolder().getAbsolutePath() + "/" + path);
+        if (setup)
+            setup();
+        if (copyDefaults)
+            copyDefaults();
+    }
+
+    public Config(JavaPlugin plugin, MSGUtils msgInstance, String path, String filename, boolean setup, boolean copyDefaults, String defaultLocation) {
+        this.msgInstance = msgInstance;
+        this.plugin = plugin;
+        this.filename = filename;
+        this.folder = new File(plugin.getDataFolder().getAbsolutePath() + "/" + path);
+        if (setup)
+            setup();
+        if (copyDefaults)
+            copyDefaults(defaultLocation);
+
+    }
+
+    public void setup() {
+        file = new File(folder, filename);
+
+        if (!folder.exists())
+            folder.mkdirs();
 
         if (!file.exists()) {
             try {
+                //noinspection ResultOfMethodCallIgnored
                 file.createNewFile();
             } catch (IOException ignored) {
+                msgInstance.sendConsoleMessage("&cCouldn't create " + filename + "! Skipping config file!");
             }
         }
         config = YamlConfiguration.loadConfiguration(file);
+    }
+
+    // TODO: 21.08.2020 copy defaults not working right now! 
+
+    public void copyDefaults() {
+        Reader defConfigStream = new InputStreamReader(plugin.getResource(filename), StandardCharsets.UTF_8);
+        YamlConfiguration defConfig = YamlConfiguration.loadConfiguration(defConfigStream);
+        config.setDefaults(defConfig);
+        config.options().copyDefaults(true);
+    }
+
+    public void copyDefaults(String location) {
+        Reader defConfigStream = new InputStreamReader(plugin.getResource(location), StandardCharsets.UTF_8);
+        YamlConfiguration defConfig = YamlConfiguration.loadConfiguration(defConfigStream);
+        config.setDefaults(defConfig);
+        config.options().copyDefaults(true);
+    }
+
+    public void copyDefaults(boolean b) {
+        Reader defConfigStream = new InputStreamReader(plugin.getResource(filename), StandardCharsets.UTF_8);
+        YamlConfiguration defConfig = YamlConfiguration.loadConfiguration(defConfigStream);
+        config.setDefaults(defConfig);
+        config.options().copyDefaults(b);
+    }
+
+    public void copyDefaults(String location, boolean b) {
+        System.out.println(location);
+        Reader defConfigStream = new InputStreamReader(plugin.getResource(location), StandardCharsets.UTF_8);
+        YamlConfiguration defConfig = YamlConfiguration.loadConfiguration(defConfigStream);
+        config.setDefaults(defConfig);
+        config.options().copyDefaults(b);
     }
 
     public FileConfiguration getConfig() {
         return config;
     }
 
-    public  void save() {
+    public void save() {
         try {
             config.save(file);
         } catch (IOException e) {
@@ -42,7 +125,7 @@ public class Config {
         }
     }
 
-    public  void reload() {
+    public void reload() {
         config = YamlConfiguration.loadConfiguration(file);
     }
 }
